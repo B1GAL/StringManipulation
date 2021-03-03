d543673f47b99462635d480b9e8cec31d075dc6c
Alexander Upton <Alexander99W@ou.edu>
2021-03-02 18:38:40 -0600

    Getting there

--- StringManipulation/Program.cs
+++ StringManipulation/Program.cs
@@ -6,12 +6,23 @@ namespace StringManipulation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Programming today is a race between software engineers striving to build bigger and better idiot-proof programs, and the universe trying to build bigger and better idiots. So far, the universe is winning.");
-            
+            string quote = "Programming today is a race between software engineers striving to build bigger and better idiot-proof programs, and the universe trying to build bigger and better idiots. So far, the universe is winning.";
+            Console.WriteLine(quote);
 
+            Console.WriteLine();
 
             Console.WriteLine("What word would you like to look for in the above sentence?");
-            Console.ReadLine();
+            string searchFor = Console.ReadLine();
+
+            Console.WriteLine("What word would you like to replace it with?");
+            string ReplaceWord = Console.ReadLine();
+
+
+
+
+            Console.WriteLine($"Sorry, I could not find the word {ReplaceWord}");
+            
+            
 
 
 
