0d3c6a3e7d84d9b1ca3d3f271650efa31e6959e8
Alexander Upton <Alexander99W@ou.edu>
2021-03-02 18:12:12 -0600

    Add project files.

---
+++ StringManipulation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace StringManipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programming today is a race between software engineers striving to build bigger and better idiot-proof programs, and the universe trying to build bigger and better idiots. So far, the universe is winning.");
+            
+
+
+            Console.WriteLine("What word would you like to look for in the above sentence?");
+            Console.ReadLine();
+
+
+
+
+
+
+        }
+    }
+}
