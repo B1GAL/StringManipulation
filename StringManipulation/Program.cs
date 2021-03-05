7f117300ec6b7d3b366ffd5f82567fe151da024e
Alexander Upton <Alexander99W@ou.edu>
2021-03-04 18:18:18 -0600

    partial

--- StringManipulation/Program.cs
+++ StringManipulation/Program.cs
@@ -6,19 +6,32 @@ namespace StringManipulation
     {
         static void Main(string[] args)
         {
-            string quote = "Programming today is a race between software engineers striving to build bigger and better idiot-proof programs, and the universe trying to build bigger and better idiots. So far, the universe is winning.";
+            string s1 = "Programming today is a race between software engineers striving to build bigger and better idiot-proof programs, and the universe trying to build bigger and better idiots. So far, the universe is winning.";
             Console.WriteLine(quote);
 
-            Console.WriteLine();
+            Console.WriteLine("What word would you like to look for in the sentence?");
+            string searchFor = "What word would you like to look for in the sentence?";
 
-            Console.WriteLine("What word would you like to look for in the above sentence?");
-            string searchFor = Console.ReadLine();
-            string oldWord = "build";
-            string newWord = "create";
-            bool b = quote.Contains(oldWord);
-            Console.WriteLine("What word would you like to replace it with?");
 
-            Console.WriteLine();
+            bool b = s1.Contains()
+
+            for (s1 i = 0; i < length; i++)
+            {
+
+            }
+            
+            //Console.WriteLine();
+
+           
+            //
+            //bool b = s1.Contains(s1);
+            //Console.WriteLine(b);
+
+            //string newWord = "create";
+            //newWord.Replace(oldWord);
+            //Console.WriteLine("What word would you like to replace it with?");
+            //Console.ReadLine();
+            //Console.WriteLine();
 
 
 
