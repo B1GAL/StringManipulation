0bec6d8237c520c307d8d5798c1f10efdd828e46
Alexander Upton <Alexander99W@ou.edu>
2021-03-04 18:27:58 -0600

    For loop

--- StringManipulation/Program.cs
+++ StringManipulation/Program.cs
@@ -7,18 +7,19 @@ namespace StringManipulation
         static void Main(string[] args)
         {
             string s1 = "Programming today is a race between software engineers striving to build bigger and better idiot-proof programs, and the universe trying to build bigger and better idiots. So far, the universe is winning.";
-            Console.WriteLine(quote);
+            Console.WriteLine(s1);
 
             Console.WriteLine("What word would you like to look for in the sentence?");
             string searchFor = "What word would you like to look for in the sentence?";
 
 
-            bool b = s1.Contains()
 
-            for (s1 i = 0; i < length; i++)
+            for (s1.Contains(searchFor));
             {
-
+                
             }
+
+           
             
             //Console.WriteLine();
 
