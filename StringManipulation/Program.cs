01007ceb953de32241e3f51f2b6baa7f3f440127
Alexander Upton <Alexander99W@ou.edu>
2021-03-05 20:25:23 -0600

    Done

--- StringManipulation/Program.cs
+++ StringManipulation/Program.cs
@@ -9,35 +9,47 @@ namespace StringManipulation
             string s1 = "Programming today is a race between software engineers striving to build bigger and better idiot-proof programs, and the universe trying to build bigger and better idiots. So far, the universe is winning.";
             Console.WriteLine(s1);
 
+            Console.WriteLine();
+
             Console.WriteLine("What word would you like to look for in the sentence?");
-            string searchFor = "What word would you like to look for in the sentence?";
+            string searchFor = Console.ReadLine();
 
+            Console.WriteLine($"What word would you like to replace {searchFor} with?");
+            string newWord = Console.ReadLine();
 
-
-            for (s1.Contains(searchFor));
+            bool b = s1.Contains(searchFor);
+            if (b)
             {
-                
+                Console.WriteLine(s1.Replace(searchFor, newWord));
             }
+                 else
+                 {
+                      Console.WriteLine($"Sorry, I could not find the word {searchFor}.");
+
+                    for (int i = searchFor.Length-1; i >= 0; i--)
+                     {
+                         Console.WriteLine($"{searchFor[i]}");
+                     }
+
+
+                 }
+                       
+            
+        
 
            
             
-            //Console.WriteLine();
-
-           
-            //
             //bool b = s1.Contains(s1);
             //Console.WriteLine(b);
 
             //string newWord = "create";
             //newWord.Replace(oldWord);
-            //Console.WriteLine("What word would you like to replace it with?");
-            //Console.ReadLine();
-            //Console.WriteLine();
+           
 
 
 
 
-            Console.WriteLine($"Sorry, I could not find the word {searchFor}");
+            
             
             
             
